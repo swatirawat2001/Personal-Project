63d255adffff255ab224c845810474eda2746159
swatirawat2001 <rawatswati356@gmail.com>
2025-06-07 19:23:11 +0530

    Quiz App

---
+++ quiz/script.js
@@ -0,0 +1,138 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answer: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answer: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "What is H2O?",
+        answer: [
+            {text: "Oxygen", correct: false},
+            {text: "Water", correct: true},
+            {text: "Hydrogen", correct: false},
+            {text: "Salt", correct: false},
+        ]
+    },
+    {
+        question: "Which vitamin is produced from sunlight?",
+        answer: [
+            {text: "Vitamin A", correct: false},
+            {text: "Vitamin B", correct: false},
+            {text: "Vitamin D", correct: true},
+            {text: "Vitamin C", correct: false},
+        ]
+    },
+    {
+        question: "Who is the founder of Microsoft?",
+        answer: [
+            {text: "Elon Musk", correct: false},
+            {text: "Steve jobs", correct: false},
+            {text: "Bill Gates", correct: true},
+            {text: "Mark Zuckerberg", correct: false},
+        ]
+    },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+
+        }
+        button.addEventListener("click", selectAnswer);
+    }); 
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectBtn.classList.add("correct");
+        score++;
+
+    }else{
+        selectBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+       
+         startQuiz();
+    }
+     
+
+});
+
+ startQuiz();
